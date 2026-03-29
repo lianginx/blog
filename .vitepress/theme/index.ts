c067e0d61ada0f98c8be5a66e3021949332f4d3d
Liang <liang@in-x.cc>
2026-03-30 01:17:20 +0800

    删除废弃组件

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -6,9 +6,6 @@ import BackTop from './components/BackTop.vue'
 import DialogPreviewImg from './components/dialog/PreviewImg.vue'
 import FootBar from './components/FootBar.vue'
 import HomeLayout from './components/HomeLayout.vue'
-import IconClose from './components/icon/close.vue'
-import IconSearch from './components/icon/search.vue'
-import IconTop from './components/icon/top.vue'
 import ListBlog from './components/ListBlog.vue'
 import ListPhoto from './components/ListPhoto.vue'
 import NavBar from './components/NavBar.vue'
@@ -35,9 +32,6 @@ export default {
       .component('ListBlog', ListBlog)
       .component('ListPhoto', ListPhoto)
       .component('PhotoSection', PhotoSection)
-      .component('IconClose', IconClose)
-      .component('IconSearch', IconSearch)
-      .component('IconTop', IconTop)
       .component('DialogPreviewImg', DialogPreviewImg)
       .component('NotFound', NotFound)
   },
