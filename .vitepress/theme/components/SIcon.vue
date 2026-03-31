de8962f3cb945397dec68113c581ec27ffc42de3
Liang <liang@in-x.cc>
2026-03-31 19:06:04 +0800

    simple-icon 图标专属组件

---
+++ .vitepress/theme/components/SIcon.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import type { SimpleIcon } from 'simple-icons'
+
+defineProps<{
+  icon: SimpleIcon
+}>()
+</script>
+
+<template>
+  <svg viewBox="0 0 24 24" fill="currentColor">
+    <path :d="icon.path" />
+  </svg>
+</template>
