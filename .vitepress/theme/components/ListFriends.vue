f26199b9ff7794ba2060ea31f3d89f0dd5d6549b
Liang <liang@in-x.cc>
2026-07-21 15:21:37 +0800

    feat: 支持友链头像亮暗主题切换

--- .vitepress/theme/components/ListFriends.vue
+++ .vitepress/theme/components/ListFriends.vue
@@ -1,9 +1,17 @@
 <script setup lang="ts">
-import type { ThemeConfig } from '#types'
+import type { FriendAvatar, ThemeConfig } from '#types'
 
-const { theme } = useData<ThemeConfig>()
+const { theme, isDark } = useData<ThemeConfig>()
 
 const applyUrl = `https://github.com/lianginx/blog/issues/new?template=申请友链模板.md`
+
+function getAvatarUrl(avatar?: string | FriendAvatar) {
+  if (!avatar)
+    return undefined
+  if (typeof avatar === 'string')
+    return avatar
+  return isDark.value ? avatar.dark : avatar.light
+}
 </script>
 
 <template>
@@ -11,18 +19,18 @@ const applyUrl = `https://github.com/lianginx/blog/issues/new?template=申请友
     <div class="grid cols-1 sm:cols-2 gap-y-6 sm:gap-x-3">
       <a
         v-for="friend in theme.friends?.items" :key="friend.link"
-        class="flex space-x-3 hover:decoration-none transition-shadow duration-200"
+        class="group flex items-center space-x-3 hover:decoration-none transition-shadow duration-200"
         :href="friend.link"
         target="_blank"
       >
         <div class="size-16 rounded-full overflow-hidden">
-          <img v-if="friend.avatar" class="size-full object-cover" :src="friend.avatar" alt="avatar">
+          <img v-if="friend.avatar" class="size-full object-cover" :src="getAvatarUrl(friend.avatar)" alt="avatar">
           <div v-else class="flex items-center size-full text-xl justify-center bg-[--vp-c-default-soft]">
             {{ friend.name.charAt(0) }}
           </div>
         </div>
         <div class="flex-1">
-          <div class="line-clamp-1">{{ friend.name }}</div>
+          <div class="group-hover:underline line-clamp-1">{{ friend.name }}</div>
           <div class="line-clamp-2 text-xs text-[--vp-c-text-2] mt-1">
             {{ friend.desc ?? friend.link }}
           </div>
--- .vitepress/theme/types/friends.ts
+++ .vitepress/theme/types/friends.ts
@@ -6,5 +6,10 @@ export interface FriendItem {
   name: string
   link: string
   desc?: string
-  avatar?: string
+  avatar?: string | FriendAvatar
+}
+
+export interface FriendAvatar {
+  light: string
+  dark: string
 }
